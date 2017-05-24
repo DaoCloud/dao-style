3a515596be4c7e505f706f3aa24df8f0230f62be
Cisy <cisy.zhu@daocloud.io>
2017-05-24 11:13:57 +0800

    chore(*): 将 dão-color.scss 单独打包

--- build/build-style.js
+++ build/build-style.js
@@ -6,15 +6,17 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 
 // 编译less
-gulp.task('css', function () {
-    gulp.src('../src/components/index.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie > 8']
-        }))
-        .pipe(cleanCSS())
-        .pipe(rename('dao-style.css'))
-        .pipe(gulp.dest('../dist/styles'));
+gulp.task('css', function() {
+  gulp.src('../src/components/dao-color.scss')
+    .pipe(gulp.dest('../dist/styles'));
+  gulp.src('../src/components/index.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie > 8']
+    }))
+    .pipe(cleanCSS())
+    .pipe(rename('dao-style.css'))
+    .pipe(gulp.dest('../dist/styles'));
 });
 
 // 拷贝字体文件
