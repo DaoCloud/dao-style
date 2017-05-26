bcf41ad1f7370846efa4ca2a6e04a29e6bba6cd5
Vivian <vivian.zhang@daocloud.io>
2017-05-27 00:48:57 +0800

    [feature](slider):add slider, add props onChange

---
+++ examples/routers/slider.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h2>连续值的</h2>
+    <dao-slider
+      v-model="valueA"
+      :range="rangeA"
+      :onChange="handleChange"
+    >
+    </dao-slider>
+    <div class="show">
+      outside Value: {{this.valueA}}
+    </div>
+    <h2>离散值的</h2>
+    <dao-slider
+      v-model="valueB"
+      :range="rangeB"
+      :step="step"
+      :onChange="handleChange"
+    >
+    </dao-slider>
+    <div class="show">
+      outside Value: {{this.valueB}}
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        valueA: 28,
+        rangeA: [20, 100],
+        valueB: 28,
+        rangeB: [20, 100],
+        step: 7,
+      };
+    },
+    methods: {
+      handleChange() {
+        console.log('value change!');
+      }
+    },
+  };
+</script>
+<style lang="scss">
+.show {
+  display: inline-block;
+  padding: 10px;
+  margin-top: 20px;
+  line-height: 1.5;
+  border: 1px solid #eee;
+  font-size: 18px;
+  font-weight: 500;
+}
+</style>
---
+++ src/components/dao-slider/dao-slider.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="dao-slider">
+    Dao Slider
+    <div
+      class="dao-slider-track"
+      @click.prevent="onHandleClick"
+      ref="track">
+      <div
+        class="dao-slider-track__fill"
+        :style="{width: curtPos*100 + '%'}"
+        >
+        <div
+          class="dao-slider-track__flag"
+          @mousedown.prevent="onHandleMouseDown"
+          >
+        </div>
+      </div>
+      <div
+        class="dao-slider-stop"
+        v-show="step"
+        :style="{left: stop * 100 + '%'}"
+        v-for="stop in stops">
+      </div>
+    </div>
+    <div class="show">
+      Inside Pos: {{this.curtPos}}<br/>
+      Inside Value: {{this.value}}
+    </div>
+  </div>
+</template>
+<script>
+  import { getStyle } from '../../utils/assist';
+
+  export default {
+    name: 'DaoSlider',
+    props: {
+      range: {
+        type: Array,
+        default: [0, 10],
+        required: true,
+      },
+      step: {
+        type: Number,
+        required: false,
+        validator(val) {
+          return val > 0;
+        }
+      },
+      value: {
+        type: Number,
+        default: 0,
+      },
+      onChange: Function,
+    },
+    data() {
+      return {
+        isDragging: false,
+        curtPos: 0,
+        sliderWidth: 0,
+        startX: null,
+        trackLeft: null,
+      };
+    },
+    computed: {
+      stops() {
+        if (!this.step) {
+          return false;
+        }
+
+        const stopCount = (this.range[1] - this.range[0]) / this.step;
+        const stepWidth = 1/stopCount;
+        const result = [0];
+
+        for (let i = 1; i < stopCount; i++) {
+          result.push(i * stepWidth);
+        }
+        result.push(1);
+        return result;
+      }
+    },
+    methods: {
+      // drag
+      onHandleMouseDown(e) {
+        this.onHandleDragStart(e);
+        window.addEventListener('mousemove', this.onHandleDragging);
+        window.addEventListener('mouseup', this.onHandleDragEnd);
+      },
+      onHandleDragStart(e) {
+        this.isDragging = true;
+        this.startX = e.clientX;
+      },
+      onHandleDragging(e) {
+        // 右滑 > 0
+        const diff = e.clientX - this.startX;
+
+        if (this.curtPos >= 1 && diff > 0){
+          this.curtPos = 1;
+          return;
+        }
+        if (this.curtPos < 0 && diff < 0) {
+          this.curtPos = 0;
+          return;
+        }
+        this.curtPos = this.curtPos + diff/this.sliderWidth;
+        this.startX = e.clientX;
+      },
+      onHandleDragEnd(e) {
+        console.log('drag end');
+
+        if (!this.step) {
+          this.startX = e.clientX;
+        } else {
+          this.onHandleClick(e);
+        }
+        window.removeEventListener('mousemove', this.onHandleDragging);
+        window.removeEventListener('mouseup', this.onHandleDragEnd);
+        this.isDragging = false;
+        if (!this.step) {
+          this.onChange();
+        }
+      },
+      // track 的 click 事件
+      onHandleClick(e) {
+        const newPos = this.curtPos + (e.clientX - this.startX)/this.sliderWidth;
+
+        if (newPos < 0 || newPos > 1) return;
+
+        if (this.step && this.stops) {
+          this.getCurtPosInSteps(newPos);
+        } else {
+          this.curtPos = newPos;
+          this.startX = e.clientX;
+        }
+      },
+      getCurtPosInSteps(newPos) {
+        if (!this.stops || !this.step) return;
+
+        let newPosArray = [];
+        const helfStepWidth = this.step / (this.range[1] - this.range[0]) / 2;
+        newPosArray = this.stops.filter((stop, idx, array) =>
+          ((stop - helfStepWidth) <= newPos) && (newPos < (stop + helfStepWidth)));
+        this.curtPos = newPosArray[0];
+        this.startX = this.trackLeft + this.curtPos * this.sliderWidth;
+      },
+      // value -> position
+      updateCurPosition(val) {
+        const newPos = (val - this.range[0])/(this.range[1] - this.range[0]);
+        return (newPos >= 0 && newPos <= 1) ? newPos : null;
+      },
+      // position -> value
+      updateNewVal(pos) {
+        const newVal = pos * (this.range[1] - this.range[0]) + this.range[0];
+        return newVal;
+      },
+    },
+    watch: {
+      curtPos(newVal) {
+        if (newVal >= 0 && newVal <= 1) {
+          this.$emit('input', this.updateNewVal(newVal));
+        }
+      },
+      value(newVal) {
+        if (!newVal) {
+          return;
+        }
+        const newPosition = this.updateCurPosition(newVal);
+        if (newPosition !== this.curtPos) {
+          this.curtPos = newPosition;
+        }
+        if (!this.isDragging) {
+          this.onChange();
+        }
+      },
+    },
+    mounted() {
+      this.sliderWidth = parseInt(getStyle(this.$refs.track, 'width'), 10);
+      this.$nextTick(() => {
+        this.curtPos = this.updateCurPosition(this.value);
+        if (this.step && this.value) {
+          this.getCurtPosInSteps(this.curtPos);
+        }
+
+        this.trackLeft = this.$refs.track.offsetLeft;
+        this.startX = this.trackLeft + this.curtPos * this.sliderWidth;
+      });
+    },
+  };
+</script>
+<style scoped lang="scss">
+  @import "./dao-slider.scss";
+</style>
