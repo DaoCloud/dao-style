0017cb7bfe3d3331570f3cda7e3d040f34bcf026
Domon Ji <646953127@qq.com>
2017-08-09 11:04:10 +0800

    fix(setting-layout): :bug: 修复了setting-layout切换时slots解析错误的问题

--- src/components/dao-setting-layout/dao-setting-item.vue
+++ src/components/dao-setting-layout/dao-setting-item.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="dao-setting-item">
-    <div class="dao-setting-label" v-if="label">
+    <div class="dao-setting-label">
       <slot name="label"></slot>
     </div>
     <div class="dao-setting-content">
       <slot name="content"></slot>
-      <div class="helper-text" v-if="contentHelper">
+      <div class="helper-text">
         <slot name="content-helper"></slot>
       </div>
     </div>
@@ -14,11 +14,5 @@
 <script>
 export default {
   name: 'DaoSettingItem',
-  data() {
-    return {
-      label: this.$slots.label,
-      contentHelper: this.$slots['content-helper'],
-    };
-  },
 };
 </script>
