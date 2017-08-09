8fbae7555661ee7f1b6fb0a3e4605fae79df06c0
Keifer Gu <keifergu@gmail.com>
2017-08-09 14:57:18 +0800

    [fix] dao-setting-layout: 修复layout切换时slots错误显示的问题

--- src/components/dao-setting-layout/dao-setting-section.vue
+++ src/components/dao-setting-layout/dao-setting-section.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="dao-setting-section">
-    <div class="dao-setting-title" v-if="sectionTitle">
+    <div class="dao-setting-title" v-if="sectionTitle()">
       <slot name="section-title"></slot>
-      <div class="helper-text" v-if="sectionTitleHelper">
+      <div class="helper-text" v-if="sectionTitleHelper()">
         <slot name="section-title-helper"></slot>
       </div>
     </div>
     <slot>
       <div class="dao-setting-item">
-        <div class="dao-setting-label" v-if="label">
+        <div class="dao-setting-label" v-if="label()">
           <slot name="label"></slot>
         </div>
         <div class="dao-setting-content">
           <slot name="content"></slot>
-          <div class="helper-text" v-if="contentHelper">
+          <div class="helper-text" v-if="contentHelper()">
             <slot name="content-helper"></slot>
           </div>
         </div>
@@ -26,10 +26,18 @@ export default {
   name: 'DaoSettingSection',
   data() {
     return {
-      sectionTitle: this.$slots['section-title'],
-      sectionTitleHelper: this.$slots['section-title-helper'],
-      label: this.$slots.label,
-      contentHelper: this.$slots['content-helper'],
+      sectionTitle () {
+        return this.$slots['section-title'];
+      },
+      sectionTitleHelper () {
+        return this.$slots['section-title-helper'];
+      },
+      label () {
+        return this.$slots.label;
+      },
+      contentHelper () {
+        return this.$slots['content-helper'];
+      },
     };
   },
 };
