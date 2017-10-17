2a92c0a5e33c0614b13bfac5738585a51df3082e
MimoN <mimo.ouyang@daocloud.io>
2017-10-17 11:59:58 +0800

    feat(draggable): 完成拖拽排序

--- examples/routers/drag-and-drop.vue
+++ examples/routers/drag-and-drop.vue
@@ -4,11 +4,13 @@
     <div v-dao-droppable>
       <div class="drag-item" data-key="1" v-dao-draggable="{disabled: show}">Dragger 1</div>
       <div class="drag-item" data-key="2" v-dao-draggable>Dragger 2</div>
-      111
+      <span>111</span>
       <div class="drag-item" data-key="trash" v-dao-draggable>
-        <svg class="icon">
-          <use xlink:href="#icon_trash"></use>
-        </svg>
+        <div>
+          <svg class="icon">
+            <use xlink:href="#icon_trash"></use>
+          </svg>
+        </div>
       </div>
     </div>
     <div v-dao-droppable>
@@ -88,11 +90,10 @@ export default {
     cursor: move;
   }
   display: inline-block;
-  padding: 5px;
   box-shadow: 0 0 12px 2px rgba(0,0,0,0.1);
   cursor: move;
   border-radius: 4px;
-  border: 1px solid #eee;
+  // border: 1px solid #eee;
   margin: 5px;
 }
 .green{
--- src/directives/draggable/dao-draggable.js
+++ src/directives/draggable/dao-draggable.js
@@ -1,4 +1,66 @@
-import dragging from './dragging';
+import Dragging from './dragging';
+
+// 获取当前元素的父元素序列
+function getList(el) {
+  const $el = el;
+  // 获取元素的父元素
+  const $parentEl = $el.parentNode;
+  // 获取父元素下所有的子元素，并筛除不可拖动的元素
+  const $children = Array.from($parentEl.children)
+    .filter(elm => elm.getAttribute && elm.getAttribute('data-key'));
+  const list = $children.map(elm => elm.getAttribute('data-key'));
+  return list;
+}
+
+// 元素位置交换
+function changePlace(targetEl, draggingEl) {
+  // 获取父元素的拖动序列
+  const $list = getList(targetEl);
+  const targetKey = targetEl.getAttribute('data-key');
+  const draggingKey = draggingEl.getAttribute('data-key');
+  // 获取在拖动序列中的序列号
+  const targetIndex = $list.indexOf(targetKey);
+  const draggingIndex = $list.indexOf(draggingKey);
+  // 当拖动中的元素不在当前父元素序列中时，不执行换位
+  if (draggingIndex < 0) return;
+  // 当拖动元素序列大于目标元素时，放到目标元素前
+  if (draggingIndex > targetIndex) {
+    targetEl.parentNode.insertBefore(draggingEl, targetEl);
+  }
+  // 当拖动序列小于目标元素时，放到目标元素后面
+  if (draggingIndex < targetIndex) {
+    // 当前元素是最后一个元素时，直接在父元素上面 append
+    if (!targetEl.nextSibling) {
+      targetEl.parentNode.append(draggingEl);
+    } else {
+      targetEl.parentNode.insertBefore(draggingEl, targetEl.nextSibling);
+    }
+  }
+}
+
+// 拖动开始
+function onDragStart(e) {
+  const el = e.target;
+  // 获取元素的 key
+  let key = el.getAttribute('data-key');
+  // 如果元素没有 key，则给一个 Symbol 值
+  if (!key) {
+    key = Symbol('key');
+    el.setAttribute('data-key', key);
+  }
+  // 把当前元素和 key 值存储到 dragging 中
+  Dragging.setData(el, key);
+}
+
+function onDragEnter(e) {
+  // 事件冒泡序列
+  const chain = Array.from(e.path);
+  // 找到真正的可以交换位置的元素，及可拖动元素，即找到第一个有 data-key 属性的元素
+  const $el = chain
+    .filter(elm => elm.getAttribute && elm.getAttribute('data-key'))[0];
+  // 交换元素位置
+  changePlace($el, Dragging.el);
+}
 
 // 绑定事件
 function addEvents(el, binding) {
@@ -11,6 +73,10 @@ function addEvents(el, binding) {
     const draggable = !$el.dragConfig.disabled;
     $el.setAttribute('draggable', draggable);
   });
+
+  // 拖动开始事件
+  $el.addEventListener('dragstart', onDragStart);
+  $el.addEventListener('dragenter', onDragEnter);
 }
 
 // 将绑定值转换成新的对象
@@ -51,6 +117,7 @@ function bind($el, $binding) {
 function update(el, $binding) {
   const $el = el;
   $el.dragConfig = dealWithBindingValue($binding);
+  console.log($el.dragConfig)
 }
 
 export default {
