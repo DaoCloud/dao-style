3edf511bcbaea3241546b8e3749963106c9d472c
bigfly <158367670@qq.com>
2017-10-20 10:39:56 +0800

    fix(dao-info-card): 内容缩进，整体向左移动2格

--- src/components/dao-info-card/dao-info-card.vue
+++ src/components/dao-info-card/dao-info-card.vue
@@ -1,72 +1,72 @@
 <template>
   <div class="dao-infocard-container" :class="containerType">
-      <div class="dao-card-header" @click="isShow=!isShow">
-          <svg class="icon">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" v-bind:xlink:href="arrowDirect">
-            </use>
-          </svg>
-          <div class="title">
-           {{title}}
-          </div>
+    <div class="dao-card-header" @click="isShow=!isShow">
+        <svg class="icon">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" v-bind:xlink:href="arrowDirect">
+          </use>
+        </svg>
+        <div class="title">
+        {{title}}
+        </div>
+    </div>
+    <template v-if="isShow">
+      <div class="dao-infocard-main"  v-if="type==='a'">
+        <ul v-for="values in datavalue">
+          <li class="dao-key-value" v-for="(value,key,index) in values">
+            <div class="key">{{key}}:</div>
+            <div class="value">
+              <span v-show="config.openDot && config.openDotIndex==index" class="dot open"></span>
+              <span v-show="config.closeDot && config.closeDotIndex==index" class="dot close"></span>
+              <span>{{value}}</span> 
+            </div>
+          </li>    
+        </ul>
       </div>
-      <template v-if="isShow">
-        <div class="dao-infocard-main"  v-if="type==='a'">
-          <ul v-for="values in datavalue">
-            <li class="dao-key-value" v-for="(value,key,index) in values">
-              <div class="key">{{key}}:</div>
-              <div class="value">
-                <span v-show="config.openDot && config.openDotIndex==index" class="dot open"></span>
-                <span v-show="config.closeDot && config.closeDotIndex==index" class="dot close"></span>
-                <span>{{value}}</span> 
-              </div>
-            </li>    
-          </ul>
+      <div class="dao-infocard-main"  v-else-if="type==='b'">
+        <div class="dao-table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th v-for=" attr in datavalue.header" >{{attr}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in datavalue.body">
+                <td v-for="value in item">{{value}}</td>
+              </tr>
+            </tbody>  
+          </table>
         </div>
-        <div class="dao-infocard-main"  v-else-if="type==='b'">
-          <div class="dao-table-wrapper">
-            <table>
-              <thead>
-                <tr>
-                  <th v-for=" attr in datavalue.header" >{{attr}}</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in datavalue.body">
-                  <td v-for="value in item">{{value}}</td>
-                </tr>
-              </tbody>  
-            </table>
-          </div>
+      </div>
+      <div class="dao-infocard-main" v-else-if="type==='c'">
+        <ul v-for="values in datavalue.keyvalue">
+          <li class="dao-key-value" v-for="(value,key,index) in values">
+            <div class="key">{{key}}:</div>
+            <div class="value">
+              <span v-show="config.openDot && config.openDotIndex==index" class="dot open"></span>
+              <span v-show="config.closeDot && config.closeDotIndex==index" class="dot close"></span>
+              <span>{{value}}</span>
+            </div>
+          </li>    
+        </ul>
+        <div class="dao-table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th v-for=" attr in datavalue.header" >{{attr}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in datavalue.body">
+                <td v-for="(value,index) in item">
+                  <span>{{value}}</span>
+                </td>
+              </tr>
+            </tbody>  
+          </table>
         </div>
-        <div class="dao-infocard-main" v-else-if="type==='c'">
-          <ul v-for="values in datavalue.keyvalue">
-            <li class="dao-key-value" v-for="(value,key,index) in values">
-              <div class="key">{{key}}:</div>
-              <div class="value">
-                <span v-show="config.openDot && config.openDotIndex==index" class="dot open"></span>
-                <span v-show="config.closeDot && config.closeDotIndex==index" class="dot close"></span>
-                <span>{{value}}</span>
-              </div>
-            </li>    
-          </ul>
-          <div class="dao-table-wrapper">
-            <table>
-              <thead>
-                <tr>
-                  <th v-for=" attr in datavalue.header" >{{attr}}</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in datavalue.body">
-                  <td v-for="(value,index) in item">
-                    <span>{{value}}</span>
-                  </td>
-                </tr>
-              </tbody>  
-            </table>
-          </div>
-        </div>
-      </template>
+      </div>
+    </template>
   </div>
 </template>
 <script>
