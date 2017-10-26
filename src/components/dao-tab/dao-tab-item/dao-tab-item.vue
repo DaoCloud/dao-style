2e575210cdef0bf2cfb8fed202de88a3e5af4caa
vivian-xu <vivian.zxx53@gmail.com>
2017-10-26 14:24:10 +0800

    fix(tab-computed): 修复 dao-tab computed 报错

--- src/components/dao-tab/dao-tab-item/dao-tab-item.vue
+++ src/components/dao-tab/dao-tab-item/dao-tab-item.vue
@@ -12,26 +12,34 @@ export default {
       required: true,
     },
   },
-  computed: {
-    active() {
-      return this.updateActive();
-    },
+  data() {
+    return {
+      active: this.updateActive(),
+      unwatch: null,
+    };
   },
   methods: {
     updateActive: function updateActive() {
       return this.$parent.activeName === this.heading;
     },
   },
+  created() {
+    this.unwatch = this.$watch('$parent.activeName', (newV, oldV) => {
+      if (newV !== oldV) {
+        this.active = this.updateActive();
+      }
+    });
+  },
   mounted() {
     this.$parent.addTab(this);
-    this.active = this.updateActive();
   },
-  beforeDestroy () {
+  beforeDestroy() {
     // destroy 此 item 组件， parent 中的 heading 并不会减少, 需要手动去除
     if (this.$el && this.$el.parentNode) {
       this.$el.parentNode.removeChild(this.$el);
     }
     this.$parent.removeTab(this);
+    this.unwatch();
   },
 };
 </script>
