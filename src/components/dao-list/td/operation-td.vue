2798d965a329cfec4b6c6881d80b3987cb86d4f3
tanbowen <tanbowensg@gmail.com>
2017-11-17 16:04:50 +0800

    fix(dao-list): 去掉操作菜单里的 groupName

--- src/components/dao-list/td/operation-td.vue
+++ src/components/dao-list/td/operation-td.vue
@@ -6,26 +6,30 @@
       placement="bottom-start">
       <svg class="dropdown-trigger"><use xlink:href="#icon_more"></use></svg>
       <dao-dropdown-menu slot="list">
-        <template v-for="group in operations">
-          <dao-dropdown-item :is-title="true">{{group.groupName}}</dao-dropdown-item>
-          <dao-dropdown-item v-for="o in group.operations"
-            :key="o.name"
-            :is-disabled="o.disabled"
-            @click="operate(o.event)">
-            <span>{{o.name}}</span>
-            <svg v-if="o.disabled" v-dao-tooltip="o.disabledTooltip">
-              <use xlink:href :href="`#icon_${o.disabledSvg}`"></use>
-            </svg>
-          </dao-dropdown-item>
-        </template>
+        <dao-dropdown-item v-for="o in flattenedOperations"
+          :key="o.name"
+          :is-disabled="o.disabled"
+          @click="operate(o.event)">
+          <span>{{o.name}}</span>
+          <svg v-if="o.disabled" v-dao-tooltip="o.disabledTooltip">
+            <use xlink:href :href="`#icon_${o.disabledSvg}`"></use>
+          </svg>
+        </dao-dropdown-item>
       </dao-dropdown-menu>
     </dao-dropdown>
   </td>
 </template>
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'OperationTd',
   props: ['operations', 'row'],
+  computed: {
+    flattenedOperations() {
+      return _.flatten(_.map(this.operations, 'operations'));
+    },
+  },
   methods: {
     operate(event) {
       // 直接让列表组件 emit 事件，这样就不用通过列表透传事件了
