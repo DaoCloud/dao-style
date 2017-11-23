282b44d45eaad79c51c63152f62153bcb8accc15
Keifer Gu <keifergu@gmail.com>
2017-11-23 16:58:54 +0800

    refact(dao-setting-layout): 将 setting-layout 修改为函数式组件

--- src/components/dao-setting-layout/index.js
+++ src/components/dao-setting-layout/index.js
@@ -1,4 +1,4 @@
-import daoSettingLayout from './dao-setting-layout.vue';
+import daoSettingLayout from './dao-setting-layout';
 import daoSettingSection from './dao-setting-section.vue';
 import daoSettingItem from './dao-setting-item.vue';
 
