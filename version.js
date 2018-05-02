c855c2902a8cb3bd4d3816b1862de713f3f5fe5b
CIN <cin@cinlive.com>
2018-05-02 10:27:52 +0800

    修改版本到 0.0.12 并生成 changelog 文件

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // this version shows current version of project
-var VERSION = '0.0.11';
+var VERSION = '0.0.12';
 module.exports = { VERSION: VERSION };
 // ’make release‘ 依赖 console.log 把打印到标准输出, 不可删除
 console.log(VERSION);
