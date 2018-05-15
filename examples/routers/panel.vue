ed8fe10381b89f2e9dfc4362d994986a07a930a9
MimoN <mimo.ouyang@daocloud.io>
2018-05-15 11:19:51 +0800

    让 demo 正常工作

--- examples/routers/panel.vue
+++ examples/routers/panel.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-      <div class="example" v-for="demo in demos" :key="demo.size">
+      <div class="example" v-for="demo in demos" :key="demo.desc">
         <div class="top-body">
           <h1>{{demo.desc}} -> size: {{demo.size}}</h1>
         </div>
-        <dao-panel @changeTab="changeTab" :size="demo.size" class="bottom-body">
+        <dao-panel @changeTab="changeTab" :size="demo.size" class="bottom-body" :visible.sync="demo.visible">
           <dao-panel-item heading="heading1">
             <p>@content1</p>
           </dao-panel-item>
@@ -23,18 +23,22 @@ export default {
       demos: [
         {
           desc: 'demo1',
+          visible: true,
           size: 's',
         },
         {
           desc: 'demo2',
+          visible: true,
           size: 'm',
         },
         {
           desc: 'demo3',
+          visible: true,
           size: 'l',
         },
         {
           desc: 'demo4 显示指定 Tab',
+          visible: true,
           size: 's',
           active: 'heading2',
         },
