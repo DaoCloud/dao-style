b8174a0ce4919e7be86e2c48685004e1a0499e34
gezuxia <gezuxia@163.com>
2018-09-11 18:56:09 +0800

    remove package-lock.json

--- examples/view/page/components/button/index.vue
+++ examples/view/page/components/button/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="docs-button">
-      <dcos-title :name="$t('button')" desc="dao-button 是用于表示按钮的组件"></dcos-title>
+      <docs-title :name="$t('button')" desc="dao-button 是用于表示按钮的组件"></docs-title>
       <docs-section>
           <template slot="title">默认用法</template>
           <template slot="content">
             <demo-code>
               <demo1 slot="demo"></demo1>
-              <code-reader slot="code" file="components/button/demos/demo-1.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-1.vue"></code-reader>
               <md-reader slot="desc">
                   mini 版暂时只支持两种颜色：grey, blue 
               </md-reader>
@@ -19,7 +19,7 @@
           <template slot="content">
             <demo-code>
               <demo2 slot="demo"></demo2>
-              <code-reader slot="code" file="components/button/demos/demo-2.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-2.vue"></code-reader>
             </demo-code>
           </template>
       </docs-section>
@@ -29,7 +29,7 @@
           <template slot="content">
             <demo-code>
               <demo3 slot="demo"></demo3>
-              <code-reader slot="code" file="components/button/demos/demo-3.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-3.vue"></code-reader>
             </demo-code>
           </template>
       </docs-section>
@@ -39,7 +39,7 @@
           <template slot="content">
             <demo-code>
               <demo4 slot="demo"></demo4>
-              <code-reader slot="code" file="components/button/demos/demo-4.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-4.vue"></code-reader>
             </demo-code>
           </template>
       </docs-section>
@@ -49,7 +49,7 @@
           <template slot="content">
             <demo-code>
               <demo5 slot="demo"></demo5>
-              <code-reader slot="code" file="components/button/demos/demo-5.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-5.vue"></code-reader>
             </demo-code>
           </template>
       </docs-section>
@@ -59,7 +59,7 @@
           <template slot="content">
             <demo-code>
               <demo6 slot="demo"></demo6>
-              <code-reader slot="code" file="components/button/demos/demo-6.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-6.vue"></code-reader>
             </demo-code>
           </template>
       </docs-section>
@@ -69,20 +69,20 @@
           <template slot="content">
             <demo-code>
               <demo7 slot="demo"></demo7>
-              <code-reader slot="code" file="components/button/demos/demo-7.vue"></code-reader>
+              <code-reader slot="code" file="button/demo-7.vue"></code-reader>
             </demo-code>
           </template>
       </docs-section>
   </div>
 </template>
 <script>
-  import Demo1 from './demos/demo-1.vue';
-  import Demo2 from './demos/demo-2.vue';
-  import Demo3 from './demos/demo-3.vue';
-  import Demo4 from './demos/demo-4.vue';
-  import Demo5 from './demos/demo-5.vue';
-  import Demo6 from './demos/demo-6.vue';
-  import Demo7 from './demos/demo-7.vue';
+  import Demo1 from '@demos/button/demo-1.vue';
+  import Demo2 from '@demos/button/demo-2.vue';
+  import Demo3 from '@demos/button/demo-3.vue';
+  import Demo4 from '@demos/button/demo-4.vue';
+  import Demo5 from '@demos/button/demo-5.vue';
+  import Demo6 from '@demos/button/demo-6.vue';
+  import Demo7 from '@demos/button/demo-7.vue';
   export default {
     data() {
       return {};
