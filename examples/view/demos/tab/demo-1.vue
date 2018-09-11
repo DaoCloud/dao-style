c0295ef5fe31a8f01b28b0d130884b6ad0fd8372
vivian-xu <vivian.zxx53@gmail.com>
2018-09-12 00:08:03 +0800

    docs(dao-tab):add docs、demo for dao-tab

---
+++ examples/view/demos/tab/demo-1.vue
@@ -0,0 +1,10 @@
+<template>
+  <dao-tab>
+    <dao-tab-item heading="标签1">
+      <h1>标签1 内容</h1>
+    </dao-tab-item>
+    <dao-tab-item heading="标签2">
+      <h1>标签2 内容</h1>
+    </dao-tab-item>
+  </dao-tab>
+</template>
---
+++ examples/view/demos/tab/demo-3.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="demo">
+    <div class="show-block">
+      <button class="dao-btn btn-sm blue" @click.stop="changeTab">go to heading2 </button>
+      <span>当前 tab：{{currentTab}}</span>
+    </div>
+    <dao-tab :currentTab.sync="currentTab">
+      <dao-tab-item heading="标签1">
+        <h1>标签1 内容</h1>
+      </dao-tab-item>
+      <dao-tab-item heading="heading2">
+        <h1>heading2 内容</h1>
+      </dao-tab-item>
+      <dao-tab-item heading="哈哈哈">
+        <h1>哈哈哈我是 哈哈哈 的内容</h1>
+      </dao-tab-item>
+    </dao-tab>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Demo3',
+  data() {
+    return {
+      currentTab: '',
+    };
+  },
+  methods: {
+    changeTab() {
+      this.currentTab = 'heading2';
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.show-block {
+  margin-bottom: 20px;
+  .dao-btn {
+    margin-right: 10px;
+  }
+}
+</style>
