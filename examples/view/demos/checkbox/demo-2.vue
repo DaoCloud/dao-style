031c3f0d0655f49a521686179fa1eaef384f40a6
AliceYuling <yyl1215@126.com>
2018-09-15 22:00:13 +0800

    [docs] add demos of dao-checkbox

---
+++ examples/view/demos/checkbox/demo-2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="dao-checkbox-group checkbox-inline">
+      <div class="dao-checkbox">
+        <label>
+          <input type="checkbox" checked>
+          option 1
+        </label>
+      </div>
+      <div class="dao-checkbox">
+        <label>
+          <input type="checkbox">
+          option 2
+        </label>
+      </div>
+      <div class="dao-checkbox disabled">
+        <label>
+          <input type="checkbox" checked disabled>
+          disable option
+        </label>
+      </div>
+      <div class="dao-checkbox disabled">
+        <label>
+          <input type="checkbox" disabled>
+          disable option
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Demo2',
+};
+</script>
--- examples/view/page/components/checkbox/index.vue
+++ examples/view/page/components/checkbox/index.vue
@@ -1,68 +1,42 @@
 <template>
-  <div>
-    <h1>普通版</h1>
-    <div class="dao-checkbox-group">
-      <div class="dao-checkbox">
-        <label>
-          <input type="checkbox" checked>
-          option 1
-        </label>
-      </div>
-      <div class="dao-checkbox">
-        <label>
-          <input type="checkbox">
-          option 2
-        </label>
-      </div>
-      <div class="dao-checkbox disabled">
-        <label>
-          <input type="checkbox" checked disabled>
-          disable option
-        </label>
-      </div>
-      <div class="dao-checkbox disabled">
-        <label>
-          <input type="checkbox" disabled>
-          disable option
-        </label>
-      </div>
-    </div>
-    <br />
-    <h1> inline 版 </h1>
-    <div class="dao-checkbox-group checkbox-inline">
-      <div class="dao-checkbox">
-        <label>
-          <input type="checkbox" checked>
-          option 1
-        </label>
-      </div>
-      <div class="dao-checkbox">
-        <label>
-          <input type="checkbox">
-          option 2
-        </label>
-      </div>
-      <div class="dao-checkbox disabled">
-        <label>
-          <input type="checkbox" checked disabled>
-          disable option
-        </label>
-      </div>
-      <div class="dao-checkbox disabled">
-        <label>
-          <input type="checkbox" disabled>
-          disable option
-        </label>
-      </div>
-    </div>
+  <div class="docs-checkbox">
+    <dcos-title :name="$t('checkbox')" desc="这是一个纯样式多选框组件"></dcos-title>
+    <docs-section>
+      <template slot="title">普通版</template>
+      <template slot="content">
+        <demo-code>
+          <demo1 slot="demo"></demo1>
+          <code-reader slot="code" file="checkbox/demo-1.vue"></code-reader>
+          <md-reader slot="desc">
+            `dao-checbox` 是一个纯样式组件，它支持所有`checkbox`原生属性
+          </md-reader>
+        </demo-code>
+      </template>
+    </docs-section>
+    <docs-section>
+      <template slot="title">inline 版</template>
+      <template slot="content">
+        <demo-code>
+          <demo2 slot="demo"></demo2>
+          <code-reader slot="code" file="checkbox/demo-2.vue"></code-reader>
+          <md-reader slot="desc">
+            给`dao-checkbox`加上`class="checkbox-inline"`即可得到 inline 版本的多选框
+          </md-reader>
+        </demo-code>
+      </template>
+    </docs-section>
   </div>
 </template>
+
 <script>
-  export default {
-    data () {
-      return {};
-    },
-  };
+import Demo1 from '@demos/checkbox/demo-1';
+import Demo2 from '@demos/checkbox/demo-2';
+
+export default {
+  name: 'DocsCheckbox',
+  components: {
+    Demo1,
+    Demo2,
+  },
+};
 </script>
-<style lang="scss" scoped>
-</style>
