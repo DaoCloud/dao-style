a76c4a8eda3fa315d30a1ae4db09ec0a1490f7ef
gezuxia <gezuxia@163.com>
2018-09-18 21:59:21 +0800

    docs(dao-btn):remove spaces for dao-btn

--- examples/view/demos/button/demo-5.vue
+++ examples/view/demos/button/demo-5.vue
@@ -1,25 +1,24 @@
 <template>
-    <div>
-        <button class="dao-btn blue has-icon">
-            <svg class="icon"><use xlink:href="#icon_archive"></use></svg>
-            <span class="text">blue</span>
-        </button>
-        <button class="dao-btn blue has-icon">
-            <span class="text">blue</span>
-            <svg class="icon"><use xlink:href="#icon_archive"></use></svg>
-        </button>
-        <button class="dao-btn blue has-icons">
-            <svg class="icon"><use xlink:href="#icon_sort-desc"></use></svg>
-            <span class="text">blue</span>
-            <svg class="icon"><use xlink:href="#icon_caret-down"></use></svg>
-        </button>
-        <button class="dao-btn blue icon-btn">
-            <svg class="icon"><use xlink:href="#icon_play"></use></svg>
-        </button>
-        <button class="dao-btn opt-btn">
-            <svg class="icon"><use xlink:href="#icon_setting"></use></svg>
-            <svg class="icon"><use xlink:href="#icon_caret-down"></use></svg>
-        </button>
-    </div>
-  </template>
-  
+  <div>
+    <button class="dao-btn blue has-icon">
+      <svg class="icon"><use xlink:href="#icon_archive"></use></svg>
+      <span class="text">blue</span>
+    </button>
+    <button class="dao-btn blue has-icon">
+      <span class="text">blue</span>
+      <svg class="icon"><use xlink:href="#icon_archive"></use></svg>
+    </button>
+    <button class="dao-btn blue has-icons">
+      <svg class="icon"><use xlink:href="#icon_sort-desc"></use></svg>
+      <span class="text">blue</span>
+      <svg class="icon"><use xlink:href="#icon_caret-down"></use></svg>
+    </button>
+    <button class="dao-btn blue icon-btn">
+      <svg class="icon"><use xlink:href="#icon_play"></use></svg>
+    </button>
+    <button class="dao-btn opt-btn">
+      <svg class="icon"><use xlink:href="#icon_setting"></use></svg>
+      <svg class="icon"><use xlink:href="#icon_caret-down"></use></svg>
+    </button>
+  </div>
+</template>
