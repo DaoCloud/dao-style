e9c5ef7b7e0d27034f6295d3a1db73d0cf5ca07d
gezuxia <gezuxia@163.com>
2018-09-19 18:27:29 +0800

    dao-btn 页面添加属性高亮

--- examples/view/page/components/button/index.vue
+++ examples/view/page/components/button/index.vue
@@ -8,7 +8,7 @@
               <demo1 slot="demo"></demo1>
               <code-reader slot="code" file="button/demo-1.vue"></code-reader>
               <md-reader slot="desc">
-                  将 class 属性值设置为 dao-btn 即可实现`dao-btn`的效果，使用 class 属性来定义 button 的样式；需要注意的是， mini 版暂时只支持两种颜色：grey, blue。
+                  将 class 属性值设置为 `dao-btn` 即可实现`dao-btn`的效果，使用 class 属性来定义 button 的样式；需要注意的是， mini 版暂时只支持两种颜色：grey, blue。
               </md-reader>
             </demo-code>
           </template>
@@ -21,7 +21,7 @@
               <demo2 slot="demo"></demo2>
               <code-reader slot="code" file="button/demo-2.vue"></code-reader>
               <md-reader slot="desc">
-                使用 disabled 属性来定义按钮不可用,设置为 true 即可。
+                使用 disabled 属性来定义按钮不可用,设置为 `true` 即可。
               </md-reader>
             </demo-code>
           </template>
@@ -34,7 +34,7 @@
               <demo3 slot="demo"></demo3>
               <code-reader slot="code" file="button/demo-3.vue"></code-reader>
               <md-reader slot="desc">
-                使用 class 属性来定义按钮的尺寸,设置为 btn-sm 即可。
+                使用 class 属性来定义按钮的尺寸,设置为 `btn-sm` 即可。
               </md-reader>
             </demo-code>
           </template>
@@ -47,7 +47,7 @@
               <demo4 slot="demo"></demo4>
               <code-reader slot="code" file="button/demo-4.vue"></code-reader>
               <md-reader slot="desc">
-                使用 class 属性来定义按钮的尺寸,设置为 btn-sm; disabled 属性来定义按钮不可用，设置为 true 即可。
+                使用 class 属性来定义按钮的尺寸,设置为 `btn-sm`; disabled 属性来定义按钮不可用，设置为 `true` 即可。
               </md-reader>
             </demo-code>
           </template>
@@ -60,7 +60,7 @@
               <demo5 slot="demo"></demo5>
               <code-reader slot="code" file="button/demo-5.vue"></code-reader>
               <md-reader slot="desc">
-                使用 class 属性来定义按钮是否带图标，包含文字时可使用 has-icon (单个图标)和 has-icons (多个图标)，不包含文字时使用 icon-btn (单个图标)和 opt-btn (多个图标），同时配合`svg`使用来定义图标。
+                使用 class 属性来定义按钮是否带图标，包含文字时可使用 `has-icon` (单个图标)和 `has-icons` (多个图标)，不包含文字时使用 `icon-btn` (单个图标)和 `opt-btn` (多个图标），同时配合`svg`使用来定义图标。
               </md-reader>
             </demo-code>
           </template>
@@ -73,7 +73,7 @@
               <demo6 slot="demo"></demo6>
               <code-reader slot="code" file="button/demo-6.vue"></code-reader>
               <md-reader slot="desc">
-                使用 class 属性来定义组合按钮，设置为 dao-btn-group，同时配合`dao-btn`使用，可参照实例进行选择使用。
+                使用 class 属性来定义组合按钮，设置为 `dao-btn-group`，同时配合`dao-btn`使用，可参照实例进行选择使用。
               </md-reader>
             </demo-code>
           </template>
