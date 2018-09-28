c6b04afbf4b52306845be590900c01eda5c66c28
gezuxia <zuxia.ge@daocloud.io>
2018-09-28 19:05:26 +0800

    docs(dao-radio):删除dao-radio里将要废弃的block版和grid版demo

--- examples/view/demos/radio/demo-1.vue
+++ examples/view/demos/radio/demo-1.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <dao-radio-group>
+    <dao-radio-group type="simple">
       <dao-radio label="111" v-model="simple">1111</dao-radio>
       <dao-radio label="222" v-model="simple" :disabled="true">2222</dao-radio>
       <dao-radio label="333" v-model="simple">3333</dao-radio>
--- examples/view/page/components/radio/index.vue
+++ examples/view/page/components/radio/index.vue
@@ -15,32 +15,6 @@
     </docs-section>
 
     <docs-section>
-      <template slot="title">block版</template>
-      <template slot="content">
-        <demo-code>
-          <demo2 slot="demo"></demo2>
-          <code-reader slot="code" file="radio/demo-2.vue"></code-reader>
-          <md-reader slot="desc">
-            在 `dao-radio-group` 中绑定 `type` 为 `block`, `dao-radio` 中设置好 `headline` 和 `description`即可。
-          </md-reader>
-        </demo-code>
-      </template>
-    </docs-section>
-
-    <docs-section>
-      <template slot="title">grid版</template>
-      <template slot="content">
-        <demo-code>
-          <demo3 slot="demo"></demo3>
-          <code-reader slot="code" file="radio/demo-3.vue"></code-reader>
-          <md-reader slot="desc">
-            在 `dao-radio-group` 中绑定 `type` 为 `gird` 和绑定 `grid-col`属性, `dao-radio` 中设置好 `headline` 和 `description`即可。
-          </md-reader>
-        </demo-code>
-      </template>
-    </docs-section>
-
-    <docs-section>
       <template slot="title">
         <docs-title name="<radio/> 属性" size="sm"></docs-title>
       </template>
@@ -53,25 +27,15 @@
 
 <script>
   import Demo1 from '@demos/radio/demo-1';
-  import Demo2 from '@demos/radio/demo-2';
-  import Demo3 from '@demos/radio/demo-3';
 
   export default {
     name: 'DocsRadio',
     components: {
       Demo1,
-      Demo2,
-      Demo3,
     },
     data() {
       return {
         radioAttrs: [{
-          name: 'grid-col',
-          type: 'number',
-          desc: '启用 grid 布局，并指定列数',
-          options: [''],
-          default: '-',
-        }, {
           name: 'type',
           type: 'string',
           desc: 'radio 组的类型',
@@ -87,18 +51,6 @@
           name: 'label',
           type: ['string', 'number', 'object', '...'],
           desc: 'radio 被选中的值,必填',
-          options: ['-'],
-          default: '-',
-        }, {
-          name: 'headline',
-          type: 'string',
-          desc: '指定 radio-block版、radio-grid版中的标题,必填',
-          options: ['-'],
-          default: '-',
-        }, {
-          name: 'description',
-          type: 'string',
-          desc: '指定 radio-block版、radio-grid版中的描述,必填',
           options: ['-'],
           default: '-',
         }],
