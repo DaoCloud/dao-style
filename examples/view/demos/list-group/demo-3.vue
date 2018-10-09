a6ac33d6c86b662b0193fbdba118f2cacc8894e7
MimoN <mimo.ouyang@daocloud.io>
2018-10-09 15:17:01 +0800

    docs(list-group): add the list group usage demo and document

---
+++ examples/view/demos/list-group/demo-3.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="dao-list-group">
+    <li v-for="i in items"
+      :class="['dao-list-item', {active: item === i}]"
+      :key="i"
+      @click="item = i">
+      item{{i}}
+      <span class="icon">
+        <svg>
+          <use xlink:href="#icon_caret-right"></use>
+        </svg>
+      </span>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      item: 1,
+      items: [1,2,3],
+    };
+  },
+}
+</script>
+
