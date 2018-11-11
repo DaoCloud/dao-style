e21b7116684b29c2935f425ffd067c0d521d4108
olivewind <olive@olivewind.com>
2018-11-11 14:21:36 +0800

    release 1.0.9-rc6

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // this version shows current version of project
-var VERSION = '1.0.9-rc5';
+var VERSION = '1.0.9-rc6';
 module.exports = { VERSION: VERSION };
 // ’make release‘ 依赖 console.log 把打印到标准输出, 不可删除
 console.log(VERSION);
